7c5b30ee9e3876a1513c9e5d570b786d2315884c
Patrick Roza <contact@patrickroza.com>
2023-03-12 15:15:38 +0100

    add test with private fields

--- test/data/Data.ts
+++ test/data/Data.ts
@@ -11,22 +11,55 @@ test("Data", () => {
   expect(Equal.equals(v, v2)).toEqual(true)
 })
 
-test("Random Class", () => {
-  class ABC {
-    constructor(readonly a: number, readonly b: number) {}
-  }
-  const opt = Optic.id<{ a: number; b: number }>()
-  const v = new ABC(1, 2)
-  const v2 = Optic.replace(opt.at("a"))(1)(v)
-  const v3 = Optic.replace(opt.at("a"))(1)(v2)
+describe("Random Class", () => {
+  test("basics", () => {
+    class ABC {
+      constructor(readonly a: number, readonly b: number) {}
+    }
+    const opt = Optic.id<ABC>()
+    const v = new ABC(1, 2)
+    const v2 = Optic.replace(opt.at("a"))(1)(v)
+    const v3 = Optic.replace(opt.at("a"))(1)(v2)
 
-  expect(Equal.equals(v, v)).toEqual(true)
-  expect(Equal.equals(v, v2)).toEqual(false)
-  expect(Equal.equals(v2, v3)).toEqual(false)
-  expect(v2 instanceof ABC).toEqual(true)
-  expect(v3 instanceof ABC).toEqual(true)
-  expect(Object.getPrototypeOf(v) === Object.getPrototypeOf(v2)).toEqual(true)
-  expect(Object.getPrototypeOf(v2) === Object.getPrototypeOf(v3)).toEqual(true)
+    expect(Equal.equals(v, v)).toEqual(true)
+    expect(Equal.equals(v, v2)).toEqual(false)
+    expect(Equal.equals(v2, v3)).toEqual(false)
+    expect(v2 instanceof ABC).toEqual(true)
+    expect(v3 instanceof ABC).toEqual(true)
+    expect(Object.getPrototypeOf(v) === Object.getPrototypeOf(v2)).toEqual(true)
+    expect(Object.getPrototypeOf(v2) === Object.getPrototypeOf(v3)).toEqual(true)
+  })
+
+  test("some private", () => {
+    class ABC {
+      [Optic.cloneTrait](a: any) {
+        const cl = new ABC(a.a, a.b)
+        cl.#test = this.#test
+        return cl // Object.assign(Object.create(this), a)
+      }
+      #test: number
+      constructor(readonly a: number, readonly b: number) {
+        this.#test = 1
+      }
+
+      set test(v: number) {
+        this.#test = v
+      }
+
+      get test() {
+        return this.#test
+      }
+    }
+    const opt = Optic.id<ABC>()
+    const v = new ABC(1, 2)
+    v.test = 2
+
+    const v2 = Optic.replace(opt.at("a"))(1)(v)
+    expect(v.test).toBe(2)
+    expect(v2.test).toBe(2)
+    expect(v2 instanceof ABC).toEqual(true)
+    expect(Object.getPrototypeOf(v) === Object.getPrototypeOf(v2)).toEqual(true)
+  })
 })
 
 test("Class without Trait", () => {
