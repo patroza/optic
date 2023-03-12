40ecf70f8475545970d21b22c02989f66069b083
Patrick Roza <contact@patrickroza.com>
2023-03-12 09:06:44 +0100

    Remove no longer needed fix for Data.Class

--- test/data/Data.ts
+++ test/data/Data.ts
@@ -1,6 +1,5 @@
 import * as Data from "@effect/data/Data"
 import * as Equal from "@effect/data/Equal"
-import * as Hash from "@effect/data/Hash"
 import * as Optic from "@fp-ts/optic"
 
 test("Data", () => {
@@ -12,33 +11,8 @@ test("Data", () => {
   expect(Equal.equals(v, v2)).toEqual(true)
 })
 
-// FIX THE ISSUE WITH `Object.setPrototypeOf` in Data.Class
-export function Class<A extends Record<string, any>>() {
-  return class Class {
-    constructor(a: A) {
-      Object.assign(this, a)
-    }
-    [Hash.symbol]() {
-      return Hash.structure(this)
-    }
-    [Equal.symbol](that: any) {
-      const selfKeys = Object.keys(this)
-      const thatKeys = Object.keys(that)
-      if (selfKeys.length !== thatKeys.length) {
-        return false
-      }
-      for (const key of selfKeys) {
-        if (!(key in that && Equal.equals(this[key], that[key]))) {
-          return false
-        }
-      }
-      return true
-    }
-  } as new(args: Omit<A, keyof Equal.Equal>) => Data.Data<A>
-}
-
 test("Class without Trait", () => {
-  class ABC extends Class<{ readonly a: number; readonly b: number }>() {}
+  class ABC extends Data.Class<{ readonly a: number; readonly b: number }>() {}
   const opt = Optic.id<{ a: number; b: number }>()
   const v = new ABC({ a: 1, b: 2 })
   const v2 = Optic.replace(opt.at("a"))(1)(v)
@@ -58,7 +32,7 @@ test("Class without Trait", () => {
 })
 
 test("Class with Trait", () => {
-  class ABCWithTrait extends Class<{ readonly a: number; readonly b: number }>() {
+  class ABCWithTrait extends Data.Class<{ readonly a: number; readonly b: number }>() {
     [Optic.cloneTrait](a: any) {
       return new (this.constructor as any)(a)
       // return Object.setPrototypeOf(a, Object.getPrototypeOf(this))
